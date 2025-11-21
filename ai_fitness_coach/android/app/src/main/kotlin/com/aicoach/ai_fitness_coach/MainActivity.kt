f1e59a5b8871109099d02bd873827ca3dbaca124
Chetan Grandhe <chetangrandhe@gmail.com>
2025-11-21 00:04:29 -0600

    del all

--- ai_fitness_coach/android/app/src/main/kotlin/com/aicoach/ai_fitness_coach/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.aicoach.ai_fitness_coach
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- ai_fitness_coach/lib/presentation/widgets/glass_widgets.dart
+++
@@ -1,5 +0,0 @@
-// Export all glass morphic components
-export 'glass/glass_card.dart';
-export 'glass/glass_button.dart';
-export 'glass/glass_text_field.dart';
-export 'glass/glass_nav_bar.dart';
