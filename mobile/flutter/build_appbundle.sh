9157fd45433a19f86356f5edb4c88116a3996b7a
ChetanG <chetangrandhe@gmail.com>
2026-04-10 14:37:55 -0500

    up

--- mobile/flutter/build_appbundle.sh
+++ mobile/flutter/build_appbundle.sh
@@ -39,13 +39,8 @@ $FLUTTER_PATH pub get
 
 # Build the app bundle
 echo -e "${YELLOW}Building app bundle (${BUILD_MODE})...${NC}"
-# REVENUECAT_GOOGLE_KEY must be set as env var before running this script
-# export REVENUECAT_GOOGLE_KEY=goog_YourKeyHere
-if [ -z "$REVENUECAT_GOOGLE_KEY" ]; then
-    echo -e "${RED}ERROR: REVENUECAT_GOOGLE_KEY env var not set${NC}"
-    echo -e "${YELLOW}Run: export REVENUECAT_GOOGLE_KEY=goog_oWxJnYQrUSCtIxMqTPcEPfWgBxq${NC}"
-    exit 1
-fi
+# RevenueCat Google API key
+REVENUECAT_GOOGLE_KEY="goog_oWxJnYQrUSCtIxMqTPcEPfWgBxq"
 
 $FLUTTER_PATH build appbundle --${BUILD_MODE} --dart-define=ENV=prod --dart-define=REVENUECAT_GOOGLE_KEY=$REVENUECAT_GOOGLE_KEY
 
